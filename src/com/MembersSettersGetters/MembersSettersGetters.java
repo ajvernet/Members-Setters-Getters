ca1d94737a89c0e38ffd9aa43c77316125a09747
Alexander Vernet <alexanderjvernet@gmail.com>
2016-07-25 14:12:29 -0400

    initial commit

---
+++ src/com/MembersSettersGetters/MembersSettersGetters.java
@@ -0,0 +1,164 @@
+package com.MembersSettersGetters;
+
+public class MembersSettersGetters 
+{
+
+	//Question 1
+	private float diskFileSize;
+
+	public float getDiskFileSize() 
+	{
+		return diskFileSize;
+	}
+
+	public void setDiskFileSize(float diskFileSize) 
+	{
+		this.diskFileSize = diskFileSize;
+	}
+	
+	//Question 2
+	private float numAcres;
+
+	public float getNumAcres() 
+	{
+		return numAcres;
+	}
+
+	public void setNumAcres(float numAcres) 
+	{
+		this.numAcres = numAcres;
+	}
+	
+	// Question 3
+	private String nameOfFile;
+
+	public String getNameOfFile() 
+	{
+		return nameOfFile;
+	}
+
+	public void setNameOfFile(String nameOfFile) 
+	{
+		this.nameOfFile = nameOfFile;
+	}
+	
+	// Question 4
+	private String phoneNumber;
+
+	public String getPhoneNumber() 
+	{
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(String phoneNumber) 
+	{
+		this.phoneNumber = phoneNumber;
+	}
+	
+	// Question 5
+	private String companyName;
+
+	public String getCompanyName() 
+	{
+		return companyName;
+	}
+
+	public void setCompanyName(String companyName) 
+	{
+		this.companyName = companyName;
+	}
+	
+	// Question 6
+	private String todaysDate;
+
+	public String getTodaysDate() 
+	{
+		return todaysDate;
+	}
+
+	public void setTodaysDate(String todaysDate) 
+	{
+		this.todaysDate = todaysDate;
+	}
+	
+	// Question 7
+	private boolean isSwitchOn;
+
+	public boolean isSwitchOn() 
+	{
+		return isSwitchOn;
+	}
+
+	public void setSwitchOn(boolean isSwitchOn) 
+	{
+		this.isSwitchOn = isSwitchOn;
+	}
+	
+	// Question 8
+	private float pi;
+
+	public float getPi() 
+	{
+		return pi;
+	}
+
+	public void setPi(float pi) 
+	{
+		this.pi = pi;
+	}
+	
+	// Question 9
+	public float speedOfLight;
+
+	public float getSpeedOfLight() 
+	{
+		return speedOfLight;
+	}
+
+	public void setSpeedOfLight(float speedOfLight) 
+	{
+		this.speedOfLight = speedOfLight;
+	}
+	
+	// Question 10
+	private Vehicle vehicle1 = new Vehicle();
+
+	public Vehicle getVehicle1() 
+	{
+		return vehicle1;
+	}
+
+	public void setVehicle1(Vehicle vehicle1) 
+	{
+		this.vehicle1 = vehicle1;
+	}
+	
+	// Question 11
+	private Address address1 = new Address();
+
+	public Address getAddress1() 
+	{
+		return address1;
+	}
+
+	public void setAddress1(Address address1) 
+	{
+		this.address1 = address1;
+	}
+	
+	// Question 12
+	
+	private Product[] productList = new Product[10];
+
+	public Product[] getProductList() 
+	{
+		return productList;
+	}
+
+	public void setProductList(Product[] productList) 
+	{
+		this.productList = productList;
+	}
+	
+	
+}
